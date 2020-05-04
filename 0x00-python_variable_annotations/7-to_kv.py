92866216dbe2000876cda4be7d3369abe15ed77c
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-04 16:53:35 -0700

    Modified second element of tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Basic annotations task 7"""
 from typing import Union, Tuple
-import math
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple (k,v)
     """
-    return (k, v)
+    return (k, v ** 2)
