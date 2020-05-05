50f8ca1dcdf2088ade52d0e07bf7be86caed9872
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-04 19:15:57 -0700

    Adding more documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns the length of an interable"""
     return [(i, len(i)) for i in lst]
