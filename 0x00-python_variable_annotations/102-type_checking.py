59f6ae882de40201064769bb54dbfc42957eb355
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-04 21:13:11 -0700

    Fixing more mypy errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,7 +4,7 @@ from typing import Union, Any, Mapping, TypeVar, Tuple, List
 T = TypeVar('T')
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Returns a list of by a factor
     """
     zoomed_in: List = [
@@ -14,8 +14,8 @@ def zoom_array(lst: List, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
-print(zoom_2x)
+
 zoom_3x = zoom_array(array, int(3.0))
