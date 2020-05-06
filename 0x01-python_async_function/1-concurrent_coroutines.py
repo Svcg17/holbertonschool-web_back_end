a30489e5cc285abaf1fd88ce88147db3702f1c34
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-05 19:16:08 -0700

    Changed the order of the given parameters

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """Async basics in Python task 1"""
 import asyncio
-import random
 from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(max_delay: float, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: float) -> List[float]:
     """Returns a list of delayed float values using wait_random coroutine"""
     delays: List[float] = []
-    tasks: List[float] = []
+    tasks = []
 
     for _ in range(n):
         tasks.append(asyncio.create_task(wait_random(max_delay)))
@@ -18,7 +17,6 @@ async def wait_n(max_delay: float, n: int) -> List[float]:
         delays.append(await(t))
 
     if not delays:
-        for _ in range(max_delay):
-            delays.append(0.0)
+        delays.append(0.0) * n
 
     return delays
