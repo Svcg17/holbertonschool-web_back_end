bf1e6bd359e81fb2ad93301b5510d52ee8145aa4
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-05 21:30:23 -0700

    Changing annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns a list of delayed float values using wait_random coroutine
     """
