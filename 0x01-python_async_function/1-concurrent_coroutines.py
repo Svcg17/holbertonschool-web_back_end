1a3c94ec7cfb6a62fcf1ccd5b699e7aef2a8d8cd
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-05 19:07:14 -0700

    Creating tasks and appending to list as soon as one is done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,16 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 async def wait_n(max_delay: float, n: int) -> List[float]:
     """Returns a list of delayed float values using wait_random coroutine"""
     delays: List[float] = []
+    tasks: List[float] = []
+
     for _ in range(n):
-        d = await wait_random(max_delay)
-        delays.append(d)
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    for t in asyncio.as_completed(tasks):
+        delays.append(await(t))
+
+    if not delays:
+        for _ in range(max_delay):
+            delays.append(0.0)
+
     return delays
