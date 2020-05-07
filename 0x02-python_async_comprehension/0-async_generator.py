d25f674737eed18855b48b4cad1a6bad059fca0e
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-06 17:08:38 -0700

    Yield rand num before sleeping

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import Generator
 async def async_generator() -> Generator[int, None, None]:
     """yields a random number between 0 and 10"""
     for _ in range(10):
-        await asyncio.sleep(1)
         r = random.uniform(0, 10)
         yield r
+        await asyncio.sleep(1)
