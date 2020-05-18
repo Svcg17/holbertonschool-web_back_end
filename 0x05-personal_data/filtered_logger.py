cb62d98f28c90f1253ee646c0f4867714423ba01
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-17 17:26:16 -0700

    Making a new class to format logs

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,28 @@
 """Filter logger task #1"""
 from typing import List
 import re
+import logging
+import sys
+
+
+class RedactingFormatter(logging.Formatter):
+    """A RedactingFormatter Class
+    Args:
+        fields: fields in messageto replace with redaction
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        formatter = logging.Formatter(self.FORMAT)
+        return formatter.format(record)
 
 
 def filter_datum(fields: List[str],
