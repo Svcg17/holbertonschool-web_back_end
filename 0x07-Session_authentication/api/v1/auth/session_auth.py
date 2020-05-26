1ecdaa2cc9471ad0983740b37e695e7265c8020c
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-25 22:57:53 -0700

    Adding a current_user function that returns User instance

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -29,3 +29,12 @@ class SessionAuth(Auth):
         if not session_id and not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User instance based on a cookie value
+        """
+        user = User()
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return user.get(user_id)
