0ac1dc63a1b999e2ddf8331991869f3fb8000eb4
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-25 21:40:35 -0700

    Uploading files

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -35,3 +36,12 @@ class Auth():
         """Current user method
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request based on its
+        session name
+        """
+        if not request:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
