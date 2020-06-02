b1f687dcd9ef07d30cea9943bf31e6688115f341
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-02 16:01:45 -0700

    Auth class can now validate credentials

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -4,6 +4,8 @@
 import bcrypt
 from db import DB
 from typing import TypeVar
+import uuid
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -11,7 +13,12 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
+
+    def _generate_uuid(self) -> str:
+        """Returns a string representation of a new UUID.
+        """
+        return str(uuid.uuid4())
+
     def register_user(self, email: str, password: str) -> TypeVar('User'):
         """Creates and saves a new user given an email and a password
         """
@@ -24,6 +31,16 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            password = password.encode("utf-8")
+            return (bcrypt.checkpw(password, user.hashed_password))
+        except Exception:
+            return False
 
 
 def _hash_password(password: str) -> str:
