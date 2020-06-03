443ed584684242bd6e1ff160e3ba2444e1186b67
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-02 17:59:10 -0700

    Adding a login route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic Flask App
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -34,6 +34,27 @@ def register_user() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    Creates a new session for the user and creates a session id cookie
+    JSON body:
+        - email
+        - password
+    Return:
+        - JSON payload
+    """
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+    if AUTH.valid_login(email, pwd):
+        s_id = AUTH.create_session(email)
+        res = make_response(jsonify({"email": email, "message": "logged in"}))
+        res.set_cookie("session_id", s_id)
+        return res
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
