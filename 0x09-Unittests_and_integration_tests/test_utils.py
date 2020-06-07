149fdd7e2eed707445caee00d444b75edeb33977
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-07 13:18:43 -0700

    Adding shebang and more methods to test access_nested_map method

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 """Unit tests for utils module
 """
 
+
 class TestAccessNestedMap(unittest.TestCase):
-    """ Tests for access nested map class
+    """ A class for testing access_nested_map method
     """
     @parameterized.expand([
         ("len_1", ({"a": 1}, ("a",)), 1),
@@ -13,7 +15,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ("access_nested", (({"a": {"b": 2}}, ("a", "b"))), 2)
     ])
     def test_access_nested_map(self, name, input, expected):
-        """
-        Testing for access_nested_map method
+        """Sucess testing for access_nested_map method
         """
         self.assertEqual(access_nested_map(*input), expected)
+
+    @parameterized.expand([
+        ("empty_map", ({}, ("a",)), "a"),
+        ("non_existent_path", ({"a": 1}, ("a", "b")), "b")
+    ])
+    def test_access_nested_map_exception(self, name, input, error):
+        """Testing failure of access_nested_map method
+        """
+        with self.assertRaises(KeyError):
+            try:
+                access_nested_map(*input)
+            except KeyError as e:
+                self.assertEqual(e.args[0], error)
+                raise
