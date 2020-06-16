deb70fe6bc55f699f35b17bf0dba83044461b3c5
Sofia Cheung <sofiavictoria17@gmail.com>
2020-06-16 16:35:13 -0700

    Implementing a counter for every store method call

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,22 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Counts how many times methods of the Cache class
+    have been called.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args):
+        k = method(self, args[0])
+        self._redis.incr(key)
+        return k
+
+    return wrapper
 
 
 class Cache:
@@ -15,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores input data in Redis using a random key
         """
@@ -22,7 +39,7 @@ class Cache:
         self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
         """Gets the value of a string and returns it converted to
         the right type
         """
@@ -30,3 +47,14 @@ class Cache:
             return fn(self._redis.get(key))
         else:
             return self._redis.get(key)
+
+
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
