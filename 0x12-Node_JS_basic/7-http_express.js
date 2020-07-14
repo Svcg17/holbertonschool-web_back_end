9c3ff1eedbff45224be5006c67028ebf5e88a30b
Sofia Cheung <sofiavictoria17@gmail.com>
2020-07-13 22:55:09 -0700

    Uploading tasks 4-8

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', ((req, res) => {
+  countStudents(process.argv[2])
+    .then((v) => {
+      res.write(`Number of students: ${v.counter}\n`);
+      let len = 0;
+      for (const key in v.inFields) {
+        if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
+          res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
+          if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
+          len += 1;
+        }
+      }
+      res.end();
+    })
+    .catch((e) => res.status(404).end(e));
+}));
+
+app.listen(port);
+
+module.exports = app;
