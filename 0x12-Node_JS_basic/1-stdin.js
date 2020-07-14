6846f941d12bbec03d22af91c79bc3c78e473263
Sofia Cheung <sofiavictoria17@gmail.com>
2020-07-13 23:18:06 -0700

    Reverting changes

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${name}\r`);
+  if (name) process.stdout.write(`Your name is: ${name}\n`);
 });
 // if stdout process in stream comes from the terminal, end process
 if (process.stdout.isTTY) {
