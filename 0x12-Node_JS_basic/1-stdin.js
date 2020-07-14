825c0899212aa875b4c900c812db34f366d3ec20
Sofia Cheung <sofiavictoria17@gmail.com>
2020-07-13 22:58:47 -0700

    Removed logging statements

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -4,7 +4,6 @@ process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  console.log(name)
   if (name) process.stdout.write(`Your name is: ${name}\r`);
 });
 // if stdout process in stream comes from the terminal, end process
